31ef32d53a8fd3e58ef5a6dfdb17fce0cd1c4a7e
Ashley Cutmore <ashley.cutmore13@imperial.ac.uk>
2013-12-19 23:21:00 +0000

    Initial Commit

---
+++ beLeaf/BLEFAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  BLEFAppDelegate.h
+//  beLeaf
+//
+//  Created by Ashley Cutmore on 19/12/2013.
+//  Copyright (c) 2013 DocMcs13group12. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLEFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ beLeafTests/beLeafTests.m
@@ -0,0 +1,34 @@
+//
+//  beLeafTests.m
+//  beLeafTests
+//
+//  Created by Ashley Cutmore on 19/12/2013.
+//  Copyright (c) 2013 DocMcs13group12. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface beLeafTests : XCTestCase
+
+@end
+
+@implementation beLeafTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
