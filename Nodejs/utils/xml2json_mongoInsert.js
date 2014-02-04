e8697071a9ee3d801305f58344ad5200e77c8ded
Gerard Haughian <gerard.haughian13@imperial.ac.uk>
2014-02-04 15:00:11 +0000

    can now insert multiple parsed xml files into mongo collection

--- Nodejs/utils/xml2json_mongoInsert.js
+++ Nodejs/utils/xml2json_mongoInsert.js
@@ -1,53 +1,52 @@
 var fs = require('fs'),
-	xml2js = require('xml2js'),
-	MongoClient = require('mongodb').MongoClient,	//ensure the relevant packages have been installed and node.js mongo driver installed
-	format = require('util').format;
+    xml2js = require('xml2js'),				//ensure the xml2js package has been installed 'npm install xml2js'
+    MongoClient = require('mongodb').MongoClient,	//ensure the node.js mongodb driver is installed 'npm install mongodb'
+    format = require('util').format;
 
 var table = 'plants',
-	parser = new xml2js.Parser(),			//ensure the xml2js package has been installed 'npm install xml2js'
-	mongod = 'mongodb://127.0.0.1:27017/';	//host and port of MongoDB server
-
-var NUM_PHOTOS = 55000;
+    mongod = 'mongodb://146.169.44.217:57017/',	//host and port of MongoDB server
+    conn = mongod + table;
+    parser = new xml2js.Parser();
+/*
+// on VM server
+var NUM_PHOTOS = 15030;
+var imagedb_path = '/home/gerardh/data/';
+*/
+// on graphic02
+var NUM_PHOTOS = 47815;
 var imagedb_path = '/data2/leafdb/train/';
 
 // connect to mongod server, read xml files, parse into json, make some tweaks and insert into collection
-MongoClient.connect(mongod + table, function(err, db) {
-
-	if(err) throw err;
-
-	for(var i = 0; i < NUM_PHOTOS; i++){
-
+MongoClient.connect(conn, function(err, db) {
+	if(err) console.log(err);
+	for(var i = 1; i <= NUM_PHOTOS; i++){
 		var file = imagedb_path + i + '.xml';
-
 		if (fs.existsSync(file)) {
-
+			console.log('file exists ' + file);
 			//read xml file
-			fs.readFile(file, function(err, data) {
-				
-				//parse data into json
-				parser.parseString(data, function (err, result) {
-					console.dir(result);
-					console.log('Done');
-	
-					// make some tweaks to result 	
-					var document = result.Image;
-					document.FileName = file; 
-					// do we add taxon tree at this stage ???
-	
-					// insert doc into mongo collection 
-					db.collection(table).insert(
-						document, 
-						{safe: true}, 
-						function(err, records){
-						console.log("Record added as "+records[0]._id);
-					}); 
-				
-				});
-			
+			var data = fs.readFileSync(file);
+			//parse data into json
+			parser.parseString(data, function (err, result) {
+			  // make some tweaks to result 	
+			  var document = result.Image;
+			  document.FileName = [ imagedb_path + document.FileName ]; 					
+			  console.log(document);
+			  // add taxon tree at this stage ???
+			  // document.TaxonTree = ['Plant', 'Flowering Plant'];
+			  // insert doc into mongo collection 
+			  db.collection(table).insert(
+			    document, 
+			    {safe: true}, 
+			    function(err, records){
+			      if(err) console.log(err);
+			    }
+			  ); 
 			});
-		
-		};
+		}
 	};
-
+//figure out a way to close the connection when finished...can't do it currently because parseString is an async function...needs to be sync i think...unless there are other options i am unaware of/yet to experiment with
+//	db.close();
 });
+
+
 
