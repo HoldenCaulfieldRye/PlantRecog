37de1c8f6cd38151a5569899a26b360b07e43cd4
Simon Leigh <simon.leigh13@imperial.ac.uk>
2014-02-08 12:51:10 +0000

    Updated App to parse environment config file (shared with MongoDB). Updated Environment Config files to contain Node specific initialisation data. Updated Environment Config files to run Mongo on correct ports.

--- Nodejs/AppServer/app.js
+++ Nodejs/AppServer/app.js
@@ -1,65 +1,88 @@
+/* DEBUG FLAG */
+
+var DEBUG = true;
 
 /**
  * Module dependencies.
  */
-
+var fs = require('fs');
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 
-// Code to allow connection to mongo
+/* Code to allow connection to mongo */
 var mongo = require('mongodb');
 var monk = require('monk');
 
 
-// Initialise our variables to store the various database ports and locations
-var db_host = ' ';
-var db_port = ' ';
-var db_database = ' ';
+/* Initialise our variables to store the various database ports and locations */
+var db_host = -1;
+var db_port = -1;
+var db_database = -1;
 
-// Store our command line arguments in args.
-// Note that:
-//           argv[0] is just node and that
-//           argv[1] is the path to the *.js
-
+/* 
+* Store our command line arguments in args.
+* Note that:
+*           argv[0] is just node and that
+*           argv[1] is the path to the *.js
+*/
 var args = process.argv.splice(2); 
 
-// Store the Environment required
-var runEnv = args[0];
+/* Store the Environment required */
+var confFile = args[0];
+var confArray = [];
 
-// Check what argument we were passed and connect to appropriate DB.
+/* Check what argument we were passed and parse the config file. */
 
-if (!runEnv){
-  console.log('No arguments given, I cannot initialise without knowing env vars!');
+if (!confFile){
+  console.log('No arguments given, I cannot initialise without a .conf');
+  process.exit(1);
 } 
-else if (runEnv === 'dev'){
-  db_host = '146.169.44.217';
-  db_port = '57017';
-  db_database = 'development';
+
+/* try and parse the file (use Sync readFile), catch any error */
+try {
+  confArray = fs.readFileSync(confFile).toString().split('\n');  
 }
-else if (runEnv === 'qa'){
-  db_host = '146.169.44.217';
-  db_port = '57117';
-  db_database = 'qualityAssurance';
-  console.log('Invalid arguments provided, I cannot initialise!');
+catch (err) {
+  console.log('Error parsing confFile: ' + err);
   process.exit(1);
 }
-else if (runEnv === 'prod'){
-  db_host = '146.169.44.217';
-  db_port = '57217';
-  db_database = 'production';
-  console.log('Invalid arguments provided, I cannot initialise!');
+
+/* Extract our configuration variables */
+for(var i in confArray){
+  
+  var dbSeekString = '# NODE_INI: db_database = ';
+  var hostSeekString = '# NODE_INI: db_host = ';
+  var portSeekString = 'port = ';
+  
+  if(confArray[i].substring(0, dbSeekString.length) === dbSeekString){
+    db_database = confArray[i].substring(dbSeekString.length);
+    console.log('Database to use is: ' + db_database);
+  }
+
+  if(confArray[i].substring(0, hostSeekString.length) === hostSeekString){
+    db_host = confArray[i].substring(hostSeekString.length);
+    console.log('Host to connect to is: ' + db_host);
+  }
+  
+  if(confArray[i].substring(0, portSeekString.length) === portSeekString){
+    db_port = confArray[i].substring(portSeekString.length);
+    console.log('Port to use is: ' + db_port);
+  }
+  
+}
+
+/* Exit if we could not configure */
+if(db_port === -1 || db_host === -1 || db_database === -1){
+  console.log('Invalid conf file provided, I cannot initialise!');
   process.exit(1);
 }
-else {
-  console.log('Invalid arguments provided, I cannot initialise!');
-  process.exit(1);
-}
+
 
 // Tell the user how we started up.
 
-console.log('Starting up with: ' + runEnv + ' parameters!');
+console.log('Starting up database connection now!');
 
 // Actually connect to the database.
 var db = monk(db_host + ':' + db_port + '/' + db_database);
@@ -71,13 +94,13 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon()); 
 
 // Set up our options related to uploads. We want to put images to each Env
 // in a different folder.
 app.use(express.bodyParser({ 
 	keepExtensions: true,
-	uploadDir:path.join('./uploads', runEnv)
+	uploadDir:path.join('./uploads', db_database)
 	})
 );
 
