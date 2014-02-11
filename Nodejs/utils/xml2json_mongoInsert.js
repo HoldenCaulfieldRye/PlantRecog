274db09d12b787daba50851396ccbedde8010143
Gerard Haughian <gerard.haughian13@imperial.ac.uk>
2014-02-11 11:17:03 +0000

    added comments and removed superfluous comments from code

--- Nodejs/utils/xml2json_mongoInsert.js
+++ Nodejs/utils/xml2json_mongoInsert.js
@@ -1,29 +1,21 @@
 var fs = require('fs'),
     xml2js = require('xml2js'),				//ensure the xml2js package has been installed 'npm install xml2js'
     MongoClient = require('mongodb').MongoClient,	//ensure the node.js mongodb driver is installed 'npm install mongodb'
-    format = require('util').format;			//is this needed?
 
-var table = 'plants',
+var table = 'plants',				//the name of the collection we wish to insert into
     mongod = 'mongodb://146.169.44.217:57017/',	//host and port of MongoDB server
-    conn = mongod + table;
+    conn = mongod + table;			//connection details for MongoClient.connect
     parser = new xml2js.Parser();
-/*
-// on VM server
-var NUM_PHOTOS = 15030;
-var IMAGE_DB_PATH = '/home/gerardh/data/';
-*/
-// on graphic02
-//var NUM_PHOTOS = 47815;
-var NUM_PHOTOS = 100000;
-var IMAGE_DB_PATH = '/home/gerardh/data/';
-//var IMAGE_DB_PATH = '/data2/leafdb/train/';
+
+var NUM_PHOTOS = 100000;			//the number of photos we wish to attempt to convert and save to DB
+var IMAGE_DB_PATH = '/home/gerardh/data/';	//filesystem directory where image meta-data is stored
 
 // connect to mongod server, read xml files, parse into json, make some tweaks and insert into collection
 MongoClient.connect(conn, function(err, db) {
 	if(err) console.log(err);
-	for(var i = 1; i <= NUM_PHOTOS; i++){
+	for(var i = 1; i <= NUM_PHOTOS; i++){	//iterate through NUM_PHOTOS on disk...image xml's are named 1.xml -> <NUM_PHOTOS>.xml
 		var file = IMAGE_DB_PATH + i + '.xml';
-		if (fs.existsSync(file)) {
+		if (fs.existsSync(file)) {	//check that the file exists first...do synchronously to ensure we subsequently read and parse the correct file
 			console.log('file exists ' + file);
 			//read xml file
 			var data = fs.readFileSync(file);
@@ -46,8 +38,6 @@ MongoClient.connect(conn, function(err, db) {
 			});
 		}
 	};
-//figure out a way to close the connection when finished...can't do it currently because parseString is an async function...needs to be sync i think...unless there are other options i am unaware of/yet to experiment with
-//	db.close();
 });
 
 
