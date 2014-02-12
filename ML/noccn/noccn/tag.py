25b6c1c2b51ea008ce56171a30103150750a2825
John McCormac <brendan.mccormac13@imperial.ac.uk>
2014-02-12 17:54:35 +0000

    Tagging script and new networks added

--- ML/noccn/noccn/tag.py
+++ ML/noccn/noccn/tag.py
@@ -7,6 +7,7 @@ import traceback
 import math
 import csv
 from multiprocessing import Process
+from itertools import tee, izip_longest
 import sys
 import time
 
@@ -26,6 +27,7 @@ from .script import resolve
 
 # This is used to parse the xml files
 import xml.etree.ElementTree as ET # can be speeded up using lxml possibly
+import xml.dom.minidom as minidom
 
 N_JOBS = -1
 SIZE = (64, 64)
@@ -41,15 +43,6 @@ def _process_tag_item(size,channels,name):
     except:
         return None
 
-'''
-Things that this script needs to do:
-Accepts a cgf file.
-Takes a threshold probability, a directory, a max batch size
-Also accepts the standard image size from batching config
-Search the directory folder for xml files and jpg files
-Batch the jpg files into max of max b_size numpy array batches of 256*256 (but variable length)
-Pull the output probabilities for each image
-'''
 
 def chunks(l, n):
     """ Yield successive n-sized chunks from l.
@@ -58,9 +51,15 @@ def chunks(l, n):
             yield l[i:i+n]
 
 
+def get_next(some_iterable):
+    it1, it2 = tee(iter(some_iterable))
+    next(it2)
+    return izip_longest(it1, it2)
+
+
 class Tagger(object):
-    def __init__(self, batch_size=1000, channels=3, size=SIZE,
-                 model=None, threshold=0.0, tagas=None,n_jobs=N_JOBS, more_meta=None,  **kwargs):
+    def __init__(self, batch_size=1000, channels=3, size=SIZE, model=None, threshold=0.0, 
+                 tagas=None, n_jobs=N_JOBS, more_meta=None, output_path=None, **kwargs):
         self.batch_size = batch_size
         self.channels = channels
         self.size = size
@@ -69,40 +68,60 @@ class Tagger(object):
         self.model = model
         self.threshold = threshold
         self.tagas = tagas
+        self.output_path = output_path
         vars(self).update(**kwargs)  # O_o
 
     def __call__(self, all_names_and_labels, shuffle=False):
         batch_num = 1
         batch_means = np.zeros(((self.size[0]**2)*self.channels,1))
-        count_correct = 0
-        count_incorrect = 0
-        for names_and_labels in list(chunks(all_names_and_labels,self.batch_size)):
-            print "\rCategorizing batch " + `batch_num`,
-	    rows = Parallel(n_jobs=self.n_jobs)(
-		delayed(_process_tag_item)(self.size,self.channels,name)
-		for name, label in names_and_labels)
+        self.count_correct = 0
+        self.count_incorrect = 0
+        start_time = time.clock()
+        for names_and_labels,n_l_next in get_next(list(chunks(all_names_and_labels,self.batch_size))):
+            loop_time = time.clock()
+            if batch_num == 1:
+		rows = Parallel(n_jobs=self.n_jobs)(
+		    delayed(_process_tag_item)(self.size,self.channels,name)
+		    for name, label in names_and_labels)
 	    data = np.vstack([r for r in rows if r is not None]).T
             mean = data.mean(axis=1).reshape(((self.size[0]**2)*self.channels,1))
             data = data - mean
-            tags = self.model.make_predictions(data,self.threshold)
-            for tag,(name, label) in zip(tags,names_and_labels):
-                tree = ET.parse(label)
-                root = tree.getroot()
-                actual = root.find('Content').text
-                if self.tagas != None:
-			title = ET.SubElement(root, self.tagas)
-			title.text = tag
-                        tree.write(label)
-                if tag!='Exclude' and actual!='LeafScan':
-                    if tag == actual:
-                        count_correct += 1
-                    else:
-                        count_incorrect +=1
+            self.model.start_predictions(data,self.threshold)
+            if n_l_next is not None:
+	        rows = Parallel(n_jobs=self.n_jobs)(
+		    delayed(_process_tag_item)(self.size,self.channels,name)
+		    for name, label in n_l_next)
+            tags = self.model.finish_predictions()
+            self.write_to_xml(zip(tags,names_and_labels))
             batch_num += 1
-            print 'Correct:' + `count_correct` + '\t\t\tIncorrect:' + `count_incorrect` + '\t\t\tRatio' + `float(count_correct)/(count_correct+count_incorrect)`
-            
-        print 'Tagging complete'
-        print 'Correct:' + `count_correct` + '\t\t\tIncorrect:' + `count_incorrect` + '\t\t\tRatio' + `float(count_correct)/(count_correct+count_incorrect)`
+	    print "Tagged %d images in %.02f seconds" % (len(names_and_labels),time.clock()-loop_time)
+            print 'Correct:' + `self.count_correct` + '\t\t\tIncorrect:',
+            print `self.count_incorrect` + '\t\t\tRatio',
+            print `float(self.count_correct)/(self.count_correct+self.count_incorrect)`
+        print "Tagging complete. Tagged %d images in %.02f seconds" % (len(all_names_and_labels),time.clock()-start_time)
+        print 'Correct:' + `self.count_correct` + '\t\t\tIncorrect:' + `self.count_incorrect` + '\t\t\tRatio' + `float(self.count_correct)/(self.count_correct+self.count_incorrect)`
+        
+    def prettify(self, elem):
+	"""Return a pretty-printed XML string for the Element.
+	"""
+	rough_string = ET.tostring(elem, encoding='utf8', method='xml')
+	reparsed = minidom.parseString(rough_string)
+	return reparsed
+
+    def write_to_xml(self,data):
+        for tag,(name, label) in data:
+            tree = ET.parse(label)
+            root = tree.getroot()
+            actual = root.find('Content').text
+            if self.tagas != None:
+		title = ET.SubElement(root, self.tagas)
+	 	title.text = tag
+		tree.write(self.output_path+'/'+os.path.basename(label))
+            if tag!='Exclude' and actual!='LeafScan':
+                if tag == actual:
+                    self.count_correct += 1
+                else:
+                    self.count_incorrect +=1
 
 
 class TagConvNet(convnet.ConvNet):
@@ -110,20 +129,27 @@ class TagConvNet(convnet.ConvNet):
         convnet.ConvNet.__init__(self,op,load_dic,dp_params)
 	self.softmax_idx = self.get_layer_idx('probs', check_type='softmax')
         self.tag_names = list(self.test_data_provider.batch_meta['label_names'])
-        self.tag_names.append('Exlude')
+        self.tag_names.append('Exclude')
+        self.b_data = None
+        self.b_labels = None
+        self.b_preds = None
+        self.b_threshold = None
 
-    def make_predictions(self, data, threshold):
+    def start_predictions(self, data, threshold):
 	# Run the batch through the model
-	data = np.require(data, requirements='C')
-	labels = np.zeros((1, data.shape[1]), dtype=np.single)
-	preds = np.zeros((data.shape[1], len(self.tag_names)-1), dtype=np.single)
-	self.libmodel.startFeatureWriter([data, labels, preds], self.softmax_idx)
+	self.b_data = np.require(data, requirements='C')
+	self.b_labels = np.zeros((1, data.shape[1]), dtype=np.single)
+	self.b_preds = np.zeros((data.shape[1], len(self.tag_names)-1), dtype=np.single)
+        self.b_threshold = threshold;
+	self.libmodel.startFeatureWriter([self.b_data, self.b_labels, self.b_preds], self.softmax_idx)
+
+    def finish_predictions(self):
 	self.finish_batch()
         # Process the batch
-        threshold_array = np.empty((preds.shape[0],1))
-        threshold_array.fill(threshold)
-        preds = np.hstack((preds,threshold_array))
-        return [self.tag_names[i] for i in np.nditer(preds.argmax(axis=1))]
+        threshold_array = np.empty((self.b_preds.shape[0],1))
+        threshold_array.fill(self.b_threshold)
+        self.b_preds = np.hstack((self.b_preds,threshold_array))
+        return [self.tag_names[i] for i in np.nditer(self.b_preds.argmax(axis=1))]
 
     def write_predictions(self):
         pass
@@ -175,6 +201,7 @@ def console():
         size=eval(cfg_dataset.get('size', '(64, 64)')),
         model=make_model(TagConvNet,'tag',sys.argv[1]),
         threshold=float(cfg.get('threshold','0.75')),
-        tagas=cfg.get('tagas',None)
+        tagas=cfg.get('tagas',None),
+        output_path=cfg.get('output-path',None)
         )
     create(filenames_and_labels)
