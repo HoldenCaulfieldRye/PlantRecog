956a54e606c2352773ca177234ec09f8e7aa3ab3
Stewart Douglas <stewart.douglas13@imperial.ac.uk>
2014-02-18 12:57:55 +0000

    Commented out the config_parser in graphic.js

--- Nodejs/lib/GraphicServer/testClassify.js
+++ Nodejs/lib/GraphicServer/testClassify.js
@@ -30,7 +30,7 @@ var restler = require('restler');
 
 
 fs.stat("sample.jpg", function(err, stats) {
-    restler.post("http://151.225.48.14:55581/classify", {
+    restler.post("http://146.169.44.217:55582/classify", {
         multipart: true,
         data: {
         	"_id": "52f916df5e5081df42255538",
