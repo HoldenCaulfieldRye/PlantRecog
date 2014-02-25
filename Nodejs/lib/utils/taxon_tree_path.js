62a5d3634516fbf9c5f51d80dc50b4f6df38ccd1
Gerard Haughian <gerard.haughian13@imperial.ac.uk>
2014-02-25 16:33:21 +0000

    added first attempt bucketing algo...needs testing

--- Nodejs/lib/utils/taxon_tree_path.js
+++ Nodejs/lib/utils/taxon_tree_path.js
@@ -9,12 +9,9 @@ function find_path_to_all_nodes(){
 }
 
 function traverse_update_path(path){
-	print("updating path: " + path);
 	for (var i in path){
 		var node = path[0];
-		print(node);
 		path.splice(0,1);
-		print(path);
 		db.taxonomy.update({Parent:node}, {$set:{Path:path}});
 	}
 }
@@ -26,4 +23,37 @@ function find_path_from_node(child){
 		return child + ',' + parent;
 	}
 	else return child;
+}
+
+
+function bucketing(threshold, tag, prob){
+	//find leaf nodes (easiest to find)
+	var cursor = db.taxonomy.find({Children : [ ] }, {Parent : 1, _id : 0});
+	//for each leaf node traverse its path updating the count of elements below it
+	for (var i =0; i< cursor.length();i++){
+		var parent = eval(tojson(cursor[i]["Parent"]));
+		var path = eval(tojson(cursor[i]["Path"]));
+		traverse_update_descendants_count((parent + path), tag, prob);			
+	}
+}
+
+function traverse_update_descendants_count(path, tag, prob, threshold){
+	//add condition to the following...i.e tag = X, prob = Y
+	var count_by_synset = db.plants.group({ key: {Synset_ID:1}, reduce: function(curr,res){res.count++}, initial:{count:0} })
+	var count = 0;
+	var path_index = 0;
+	for (var i in path){
+		var node = path[i];
+		//count starts at zero at leaf node and increases for each ancestor
+		count+= count_by_synset(node);
+		if(count >=threshold){
+			path_index = i;
+			}
+		//better to add to new table, but what other info do we need to include??
+		db.buckets.update({Node:node}, {$inc : { Count : count}});
+	}
+	var bucket = path[path_index];
+	for(path_index; path_index>=0; path_index--){
+		db.buckets.update({Node:path[path_index]}, {$set : { Bucket : bucket}});
+	}
 }
