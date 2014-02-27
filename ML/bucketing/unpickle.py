4dd29d543e022355bc1b2d5a86e335497a8ae01e
John McCormac <brendan.mccormac13@imperial.ac.uk>
2014-02-27 10:19:50 +0000

    Added Optimised PCA illumination augment script

--- ML/bucketing/unpickle.py
+++ ML/bucketing/unpickle.py
@@ -5,8 +5,8 @@ import tree
 def mymain():
     myfile = open('taxonomyTree.pickle','rb')
     mytree = pickle.load(myfile)
-    for keys in mytree.children:
-        print `keys` + ' : ' + `mytree.children[keys]`
+    for keys in mytree.images:
+        print `keys` + ' => images:' + `mytree.images[keys]` + '  all images:' + `mytree.all_images[keys]`
     myfile.close()
 
 
