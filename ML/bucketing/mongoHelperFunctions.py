a52d19d8d7cea403ec6c19d3ebb5bc1180ff5b59
Gerard Haughian <gerard.haughian13@imperial.ac.uk>
2014-02-27 07:59:04 +0000

    bucketing algo and python help functions complete

--- ML/bucketing/mongoHelperFunctions.py
+++ ML/bucketing/mongoHelperFunctions.py
@@ -6,9 +6,25 @@ n11665372 -> angiosperm, flowering plant
 n11552386 -> spermatophyte, phanerogam, seed plant
 n13083586 -> tracheophyte, vascular plant
 n00017222 -> plant, flora, plant life
+n11545524 -> nonflowering plant
+
+n13104059 -> tree
+n12651821 -> fruit tree
+
+n11645914 -> araucaria
+n13109733 -> angiospermous tree
 
 
+#possibilities???
+n13085113 -> weed
+n11779300 -> arum (about 25 species of flowering plants in the family Araceae) ?? too general?
+n13108662 -> gymnospermous tree (any tree of the division Gymnospermophyta)
+n11915214 -> composite plant
+n12205694 -> herb, herbaceous plant
+n13084184 -> succulent plant
 '''
+
+
 import os
 import pymongo
 from   pymongo import MongoClient
@@ -23,7 +39,8 @@ def bucketing(threshold, component, componentProb):
     bucket_cmd = "mongo localhost:57027/development --eval \"THRES=" + str(threshold) + ", TAG=\'" + component + "\', PROB=\'" + str(componentProb) + "\';\" bucketing.js"
     print bucket_cmd
     os.system(bucket_cmd)
-    res = db.plants.find({ 'Exclude' : False, 'Count' : {'$gte' : threshold}, '$where' : "this.Bucket != this.Synset_ID" } , {'Image':True, 'Species':True , '_id':False})
+    #res = db.plants.find({ 'Exclude' : False, 'Count' : {'$gte' : threshold}, '$where' : "this.Bucket != this.Synset_ID" } , {'Image':True, 'Species':True , '_id':False})
+    res = db.plants.find({ 'Exclude' : False, 'Count' : {'$gte' : threshold}} , {'Image':True, 'Species':True , '_id':False})
     print 'number of images returned: ' + str(res.count())
     for i in res:
         images.append(i['Image'])
@@ -33,12 +50,17 @@ def bucketing(threshold, component, componentProb):
 
 
 def exclude_synset(synset):
-    res = db.plants.update({'Synset_ID' : synset}, {'$set' : {'Exclue': True}},  {'multi' : True})
+    print "excluding synset " + synset
+    res = db.plants.update({'Synset_ID' : synset}, {'$set' : {'Exclude': True}}, False, {'multi' : True})
     return res
 
 
 #Example usage:
-img, spec = bucketing(500, "Leaf", 0.8)
+#img, spec = bucketing(900, "Leaf", 0.8)
 #print img
 #print spec
+
+
+#exclude_synset("n00017222")
+
 
