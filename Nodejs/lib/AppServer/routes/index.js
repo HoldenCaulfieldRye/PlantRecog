fa9d483d6019dad8f37b56147b850f6757e54472
Simon Leigh <simon.leigh13@imperial.ac.uk>
2014-02-28 11:10:23 +0000

    fixed some indentation

--- Nodejs/lib/AppServer/routes/index.js
+++ Nodejs/lib/AppServer/routes/index.js
@@ -81,47 +81,47 @@ exports.upload = function(db, configArgs) {
 	
 	    if (files){
 	    
-		/* output where we saved the file */
-		console.log("FilePath is: \n" + filePath);
+			/* output where we saved the file */
+			console.log("FilePath is: \n" + filePath);
 	    
-		// Set our collection
-		var collection = db.collection('usercollection');
+			// Set our collection
+			var collection = db.collection('usercollection');
 
-		// Submit to the DB
-		collection.insert({
-		    vm_filepath : filePath,
-		    submission_state : "File Submitted from App",
-		    submission_time : Math.round(new Date().getTime() / 1000),
-		    image_metadata : {
-			date : fields.date,
- 			latitude : fields.latitude,
-			longitude : fields.longitude
-		    },
-		    image_segment : fields.segment 
-		},
-                 {safe: true}, 
-                 function (db_err, docs) {
-		     if (db_err) {
-			 // If it failed, return error
-			 res.send("There was a problem adding the information to the database.");
-		     }
-		     else {
-			 // If it worked, return JSON object from collection to App//
-		         res.json( { "id" : docs[0]._id });
+			// Submit to the DB
+			collection.insert({
+		    	vm_filepath : filePath,
+		    	submission_state : "File Submitted from App",
+		    	submission_time : Math.round(new Date().getTime() / 1000),
+		    	image_metadata : {
+					date : fields.date,
+ 					latitude : fields.latitude,
+					longitude : fields.longitude
+		    	},
+		    	image_segment : fields.segment 
+				},
+                {safe: true}, 
+                function (db_err, docs) {
+		     		if (db_err) {
+			 			// If it failed, return error
+			 			res.send("There was a problem adding the information to the database.");
+		     			}
+		     		else {
+			 			// If it worked, return JSON object from collection to App//
+		         		res.json( { "id" : docs[0]._id });
 		         
-			 // Send the image over to the classifier
-			 restler.post(graphicServer + "/classify", {
-			     multipart: true,
-			     data: {
-				 _id: docs[0]._id,
-				 datafile: restler.file(files.datafile.path, null, files.datafile.size, null, "image/jpeg")
-			     }
-			 }).on("complete", function(data) {
-			     console.log("GraphicServer response: \n" + util.inspect(data) );
-			 });
+			 			// Send the image over to the classifier
+			 			restler.post(graphicServer + "/classify", {
+			     			multipart: true,
+			     			data: {
+				 				_id: docs[0]._id,
+				 				datafile: restler.file(files.datafile.path, null, files.datafile.size, null, "image/jpeg")
+			     			}
+			 			}).on("complete", function(data) {
+			     			console.log("GraphicServer response: \n" + util.inspect(data) );
+			 				});
 			 
-		     }
-		 });
+		     		}
+		 	});
 		
 		
 	    }
