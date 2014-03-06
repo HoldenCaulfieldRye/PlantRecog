650121984d3a9a64bf3d0d5bbcf80a4e016bb0d6
John McCormac <brendan.mccormac13@imperial.ac.uk>
2014-03-06 22:30:09 +0000

    Added test result directory

--- ML/tests/unit_tests/libs.py
+++ ML/tests/unit_tests/libs.py
@@ -3,7 +3,6 @@ def add_path():
     sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../cuda_convnet"))
     sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../noccn/noccn/"))
     sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../bucketing/"))
-    print sys.path
 add_path()
 import tag
 import mongoHelperFunctions
