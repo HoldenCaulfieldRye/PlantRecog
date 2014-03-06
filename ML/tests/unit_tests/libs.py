7370e7aca57e8dbabb9363184143cf13ea6b4e16
John McCormac <brendan.mccormac13@imperial.ac.uk>
2014-03-06 22:17:02 +0000

    Added sys path test

--- ML/tests/unit_tests/libs.py
+++ ML/tests/unit_tests/libs.py
@@ -3,6 +3,7 @@ def add_path():
     sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../cuda_convnet"))
     sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../noccn/noccn/"))
     sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../bucketing/"))
+    print sys.path
 add_path()
 import tag
 import mongoHelperFunctions
