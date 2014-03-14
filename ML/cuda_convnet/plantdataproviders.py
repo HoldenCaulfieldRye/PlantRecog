48e3fa3c079543070024af22fa1c667c2485624a
alex <adalyac@gmail.com>
2014-03-14 05:31:42 +0000

    going to switch from row-first incrementation to column-first incrementation. all working well here

--- ML/cuda_convnet/plantdataproviders.py
+++ ML/cuda_convnet/plantdataproviders.py
@@ -140,7 +140,7 @@ class AugmentLeafDataProvider(LabeledDataProvider):
                 if flip == 1:
                     patch = patch[:,:,::-1]
                 target[:,c] = patch.reshape((self.get_data_dims(),)) # typo?
-                
+                 
             if flip == 1:
                 if self.patch_idx[0] == maxX:
                     if self.patch_idx[1] == maxY:
