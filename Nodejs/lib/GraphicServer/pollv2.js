ca148ff8375c22f0b16b4a9679bca808a34e1e7b
Stewart Douglas <stewart.douglas13@imperial.ac.uk>
2014-05-12 00:17:34 +0100

    New pollv2.js file

--- Nodejs/lib/GraphicServer/pollv2.js
+++ Nodejs/lib/GraphicServer/pollv2.js
@@ -8,34 +8,137 @@ var db_host = process.argv[2];
 var db_port = process.argv[3];
 var db_database = process.argv[4];
 
-connectToMongo()
-	.then(function(db){
-		getNetImages(db)
-			.then(function(){
-		 		cosole.log("got here");
-		 	});
+console.log("db_host: " + db_host + ", db_port: " + db_port + ", db_database: " + db_database);
+
+connectToMongo(db_host,db_port,db_database, function(db){     // returns the database connection
+
+                /* Query for new images every 2 seconds*/
+	        setInterval(function(){
+		
+		    return getNewImages(db)     // returns all the relevant image documents
+			.then(function(docs,err){
+			    if(err){console.log("error: " + err)};
+			    if(docs.length != 0) {
+				return runClient(docs) // also returns the image documents
+ 				    .then(function(docs){
+					return updateClassifiedCount(db,docs) // also returns the image documents
+					    .then(function(docs){
+						return checkGroupCompletion(db,docs);
+					    });
+				    });
+			    }  // end of docs.length != 0 test
+	
+			});
+
+		}, 2000) // end of setInterval()
 	});
 
 
-function connectToMongo(){
-	var deferred = Q.defer();
-  	mongoClient = new mongo.MongoClient(new mongo.Server(db_host, db_port), {native_parser: true});
+/******************** function definitions *************************/
+
+	
+function connectToMongo(host,port,database,callback){
+        console.log("0) Running connectToMongo()");
+  	mongoClient = new mongo.MongoClient(new mongo.Server(host, port), {native_parser: true});
   	mongoClient.open(function(err, mongoClient){
-		db = mongoClient.db(db_database);
-		deferred.resolve(db);
-		return deferred.promise;
+		db = mongoClient.db(database);
+		callback(db);
 	});
-})
+};
 
 function getNewImages(db) {
 	var deferred = Q.defer();
-	db.collection('segment_images').find({"submission_state" : "File received by graphic", "image_segment": components[i]})
-								   .sort({"submission_time": -1}).limit(128)
-								   .toArray(function(err,docs){
-								   		var date = new Date();
-										console.log(date);
-                      					console.log("Return #" + docs.length + " documents.")
-                      					deferred.resolve(docs);
-                      					return deferred.promise;
-     								});
-});
+        console.log("1) Running getNewImages() ");
+	db.collection('segment_images').find({"submission_state" : "File received by graphic"})
+				       .sort({"submission_time": -1})
+	                               .limit(128)
+				       .toArray(function(err,docs){
+					   var date = new Date();
+					   console.log(date);
+                      			   console.log("Return #" + docs.length + " documents.")
+                      			   if(err){
+					       deferred.reject(new Error(err))}
+					   else{
+				               deferred.resolve(docs);
+					   }
+					});
+       return deferred.promise;
+
+};
+
+function runClient(results){
+    var deferred = Q.defer();
+    console.log("2) Running runClient()");
+    var count = 0;
+    var str = "";
+    while(count < results.length){
+	str += str + " " + results[count].graphic_filepath;
+	count++;
+    }
+    
+    if(count >= results.length){
+	exec('python ./ML/runclient.py entire' + str, function(error,stdout,sterror){
+	    if(error){
+		console.log("Error running runclient.py");
+		throw error;
+	    }
+	    else {
+		deferred.resolve(results);
+	    }
+	    });
+    }
+    return deferred.promise;
+};
+
+function updateClassifiedCount(db,results){
+    var deferred = Q.defer();
+    var count = 0;
+    console.log("3) Running updateClassifiedCount()");
+    while(count < results.length){
+	db.collection('groups').update({"_id" : new BSON.ObjectID(String(results[count].group_id))},{ $inc : { "classified_count": 1} },
+				       function(err,result){ if (err) throw err; })
+        console.log("Updated group: " + results[count].group_id)
+        db.collection('segment_images').update({"_id" : results[count]._id}, { $set : {"submission_state": "File analysed by net" } }, 
+				       function(err,rez){ if (err) throw err; })
+    count++;
+    }
+    if(count >= results.length){
+	deferred.resolve(results);
+    }
+    console.log("Returning from updateClassifiedCount()");
+    return deferred.promise;
+};
+
+function checkGroupCompletion(db,results){
+    var deferred = Q.defer();
+    console.log("4) Running checkGroupCompletion()");
+    var count = 0;
+    while(count < results.length){
+	 db.collection('groups').find({"_id" : new BSON.ObjectID(String(results[count].group_id))})
+                                .toArray(function(err,toUpdate){
+				    if(toUpdate[0].image_count == toUpdate[0].classified_count && toUpdate[0].group_status == "Complete"){ 
+					
+					    var result_set = ''; 
+					    if(toUpdate[0].leaf)   result_set = result_set + ' leaf '   + toUpdate[0].leaf 
+					    if(toUpdate[0].flower) result_set = result_set + ' flower ' + toUpdate[0].flower 
+					    if(toUpdate[0].fruit)  result_set = result_set + ' fruit '  + toUpdate[0].fruit
+					    if(toUpdate[0].entire) result_set = result_set + ' entire ' + toUpdate[0].entire 
+				    
+					    console.log("Time to exec the combine.py script.")
+				   
+					    exec("python ./ML/combine.py " + result_set, function(err,stdout,stderro){
+						
+						if(!err){
+						    db.collection('groups').update({"_id": toUpdate[0]._id},{$set: {"classification": stdout, "group_status": "Classified" }}, function(err,res){
+							console.log("Classification added to group: " + toUpdate[0]._id)
+						    })
+						}
+						else {
+						    console.log("Something went wrong with combine.py. Not update group classification.");
+						}
+					    }); // exec combine.py complete
+					}
+				    });
+	count++;
+    } // end of while()
+};
